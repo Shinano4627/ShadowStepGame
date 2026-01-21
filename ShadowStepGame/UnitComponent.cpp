77263c065f068ca85bd71602e83a0a1ae802147e
mai05202525 <as.maimai2525@gmail.com>
2026-01-21 12:00:40 +0900

    ユニットコンポネント関数仮実装

--- ShadowStepGame/UnitComponent.cpp
+++ ShadowStepGame/UnitComponent.cpp
@@ -18,3 +18,190 @@ UnitComponent::UnitComponent()
 void UnitComponent::Update()
 {
 }
+
+// ===================================================================
+// 行動可否判定
+// ===================================================================
+bool UnitComponent::CanAct() const
+{
+	//そのターンにまだ行動していない
+	return !m_hasActed && IsAlive() && !m_isDown;
+}
+
+// ===================================================================
+// 移動
+// ===================================================================
+void UnitComponent::Move(const MapPosition& target)
+{
+	//行動済みなら何もしない
+	if (!CanAct())
+		return;
+
+	//座標更新
+	m_gridPos = target;
+
+	//行動済みにする
+	m_hasActed = true;
+
+#ifdef _DEBUG
+	std::cout
+		<< "[Unit Move] ("
+		<< target.posX << ","
+		<< target.posZ << ")"
+		<< std::endl;
+#endif
+}
+// ===================================================================
+// 影移動
+// ===================================================================
+void UnitComponent::ShadowMove(const MapPosition& target)
+{
+	if (!CanAct())
+		return;
+
+	m_gridPos = target;
+
+	m_hasActed = true;
+
+#ifdef _DEBUG
+	std::cout
+		<< "[ShadowMove] pos = ("
+		<< target.posX << ","
+		<< target.posZ << ")"
+		<< std::endl;
+#endif
+}
+
+// ===================================================================
+// 影を踏まれる
+// ===================================================================
+void UnitComponent::Kill()
+{
+	m_status.hp = 0;
+	m_isDown = false;
+	m_hasActed = true;
+
+#ifdef _DEBUG
+	std::cout << "[Kill]" << std::endl;
+#endif
+}
+
+// ===================================================================
+// 攻撃
+// ===================================================================
+void UnitComponent::Attack(UnitComponent* target)
+{
+	if (!CanAct()) return;
+
+	if (target == nullptr)
+		return;
+
+	//仮ダメージ
+	const int damage = 5;
+
+	target->TakeDamage(damage);
+
+	m_hasActed = true;
+
+#ifdef _DEBUG
+	std::cout
+		<< "[Attack] tagetHP = "
+		<< target->GetHP()
+		<< std::endl;
+#endif
+}
+// ===================================================================
+// 被ダメ
+// ===================================================================
+void UnitComponent::TakeDamage(int damage)
+{
+	if (!IsAlive())
+		return;
+
+	m_status.hp -= damage;
+
+	if (m_status.hp <= 0)
+	{
+		m_status.hp = 0;
+		Down();
+	}
+
+#ifdef _DEBUG
+	std::cout
+		<< "[TakeDamage]HP = "
+		<< m_status.hp
+		<< std::endl;
+#endif 
+
+}
+// ===================================================================
+// ダウン状態
+// ===================================================================
+void UnitComponent::Down()
+{
+	if (m_isDown)
+		return;
+
+	m_isDown = true;
+	m_hasActed = true;
+
+#ifdef _DEBUG
+	std::cout << "[Down]" << std::endl;
+#endif
+}
+// ===================================================================
+// 配置
+// ===================================================================
+void UnitComponent::Place(const MapPosition& target)
+{
+	if (!CanAct())
+		return;
+
+	//マップ座標設定
+	m_gridPos = target;
+
+	m_hasActed = true;
+
+#ifdef _DEBUG
+	std::cout
+		<< "[Place] pos = ("
+		<< target.posX << ","
+		<< target.posZ << ")"
+		<< std::endl;
+#endif
+}
+
+// ===================================================================
+// 壁破壊
+// ===================================================================
+void UnitComponent::BreakWall(const MapPosition& target)
+{
+	if (!CanAct()) return;
+
+	if (m_status.model != UnitModel::UnitGiant)
+		return;
+
+	m_hasActed = true;
+
+#ifdef _DEBUG
+	std::cout
+		<< "[BreakWall] pos = ("
+		<< target.posX << ","
+		<< target.posZ << ")"
+		<< std::endl;
+#endif
+}
+
+// ===================================================================
+// 状態取得
+// ===================================================================
+bool UnitComponent::IsAlive() const
+
+{
+	return m_status.hp > 0;
+}
+
+bool UnitComponent::IsDown()const
+{
+	return m_isDown;
+}
--- ShadowStepGame/UnitComponent.h
+++ ShadowStepGame/UnitComponent.h
@@ -48,15 +48,19 @@ public:
         MapPosition pos;    //マップ上の位置
         int hp = 5;         //HP
         int speed = 5;      //素早さ
-        std::vector<MapPosition> shadowPosList; //影範囲
+        //std::vector<MapPosition> shadowPosList; //影範囲
         UnitCamp camp;      //陣営
         //UnitState state;    //状態
         UnitModel model;    //種類
     };
 private:
     UnitStatus m_status;
+    MapPosition m_gridPos;  //現在の座標
 
-    MapPosition m_gridPos;      //現在の座標
+    bool m_isAlive = true;  //生存しているか
+    bool m_isDown = false;  //ダウン中か
+
+    bool m_hasActed = false;   //行動済みか
 
 public:
  
@@ -75,7 +79,29 @@ public:
     // ===================================================================
     void Update();
 
+    // ===================================================================
+    // 行動
+    // ===================================================================
+    void Move(const MapPosition& target);        //移動
+    void ShadowMove(const MapPosition& target);  //影移動
+
+    void Kill();                                 //死亡(影を踏まれる)
+
+    void Attack(UnitComponent* target);          //攻撃
+    void TakeDamage(int damage);                 //被ダメ
+    void Down();                                 //ダウン状態
+
+    void Place(const MapPosition& target);       //配置
+
+    void BreakWall(const MapPosition& target);   //壁破壊   
+
+    bool CanAct() const;    //行動可否
+    bool IsAlive() const;   //生存判定
+    bool IsDown() const;    //ダウン判定
+
+    // ===================================================================
     // Getter / Setter
+    // ===================================================================
     UnitCamp GetCamp() const { return m_status.camp; }
     //UnitState GetState() const { return m_status.state; }
     int GetHP() const { return m_status.hp; }
