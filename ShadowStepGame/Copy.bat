b09fd7ce685207cddca47a2c6521a57c00cf0c51
Shinano <ringonado@gmail.com>
2026-01-22 18:38:53 +0900

    カーソル管理クラスを追加

--- ShadowStepGame/Copy.bat
+++ ShadowStepGame/Copy.bat
@@ -2,7 +2,7 @@ rd /s /q Framework
 xcopy /e /y C:\work\GameFramework\GameFramework\GameFramework Framework\
 robocopy /MOVE /E Framework\asset\ asset\
 rd /s /q Framework\asset\
-robocopy /MOVE /E Framework\data\ data\
+robocopy /MOVE /E /XC /XN /XO Framework\data\ data\
 rd /s /q Framework\data\
 robocopy /MOVE /E Framework\shader\ shader\
 rd /s /q Framework\shader\
