fa527ba2e01622d26e2f498907452d17dd682138
koha0713 <sakuyagames6@gmail.com>
2026-01-22 10:30:06 +0900

    System関係仮完成

--- ShadowStepGame/SceneProto.cpp
+++ ShadowStepGame/SceneProto.cpp
@@ -36,23 +36,22 @@ void SceneProto::Init()
         // マップシステム
         auto* mapSystem = FindGameObjectWithTag("System")->AddComponent<MapSystemComponent>("TestMap.csv");
         mapSystem->MakeMap(m_GameObjects);    // マップの読み込み
-        float heightMap = mapSystem->GetMapSizeHeight();
-        float widthMap = mapSystem->GetMapSizeWidth();
+        int heightMap = mapSystem->GetMapSizeHeight();
+        int widthMap = mapSystem->GetMapSizeWidth();
 
         // 太陽
         auto* sun = FindGameObjectWithTag("System")->AddComponent<SunManageComponent>(
             widthMap, heightMap, mapSystem->GetMapHeight(), mapSystem->GetMapWidth());
-        // sun->Init();
 
         // シャドウシステム
         auto* shadowSystem = FindGameObjectWithTag("System")->AddComponent<ShadowSystemComponent>();
+        
 
         // ユニットシステム
         auto* unitSystem = FindGameObjectWithTag("System")->AddComponent<UnitSystemComponent>();
 
         // ゲームシステム
         auto* gameSystem = FindGameObjectWithTag("System")->AddComponent<GameSystemComponent>();
-        gameSystem->Init();
 
         // カメラ
         auto* orbitCamera = FindGameObjectWithTag("System")->AddComponent<OrbitCameraComponent>(&m_Camera);
